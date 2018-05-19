0d9d4ea5bce4c5fa747104e85f5df0ce90d92711
Ganesha Geronimo <33540880+geronimo27@users.noreply.github.com>
2018-05-19 15:33:43 +0700

    products page controller

---
+++ product/Productpage.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Productpage extends CI_Controller {
+
+	public function index() {
+
+		#Posts Section
+		$section = "p";
+		$slug = FALSE;
+		//$data['title_post'] = 'Latest Products';
+		$data['posts'] = $this->Post_model->get_posts($slug,$section);
+		//print_r($data['posts']);
+		
+		$this->load->view('templates/header');
+		$this->load->view('products/products', $data);
+		$this->load->view('templates/footer');
+		
+	}
+
+}
